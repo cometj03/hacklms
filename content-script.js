64e8975101b5b6eda67abf7e34a9642ce68c3a80
cometj03 <harryjung1001@gmail.com>
2026-03-28 16:23:39 +0900

    feat: add complete button

--- content-script.js
+++ content-script.js
@@ -31,24 +31,59 @@ const DEBUG = true;
     const targetUrl = new URL(videoSrc).searchParams.get('TargetUrl');
     if (DEBUG) console.log('targetUrl', targetUrl); // ex) https://canvas.ssu.ac.kr/learningx/api/v1/courses/44036/sections/0/components/794113/progress?user_id=20222904&content_id=69bd616f92eb6&content_type=movie
 
-    // await fetch(`https://canvas.ssu.ac.kr/learningx/api/v1/courses/44036/attendance_items/794113`)
     const courseId = root.getAttribute('data-course_id');
     const itemId = root.getAttribute('data-item_id');
-    const userId = root.getAttribute('data-user_login');
-    console.log({courseId, itemId, userId});
+    if (DEBUG) console.log({courseId, itemId});
     
-    //     const skipBtn = document.createElement('button');
-    //     skipBtn.innerText = '학습 완료';
-    //     root.appendChild(skipBtn);
+    // 학습 진행상황 정보 가져오기
+    const check = async () => {
+        const token = document.cookie.split(';').find((s) => s.startsWith(' xn_api_token'))?.replace(' xn_api_token=', '');
+        const res = await fetch(`https://canvas.ssu.ac.kr/learningx/api/v1/courses/${courseId}/attendance_items/${itemId}`, {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        });
+        if (!res.ok) return {};
+        return await res.json();
+    };
+    const completeBtn = document.createElement('button');
+    completeBtn.innerText = '학습 완료시키기';
+    completeBtn.onclick = async () => {
+        const c = await check();
+        if (DEBUG) console.log(c);
+        const duration = c.item_content_data?.duration ?? 1;
+        if (c.attendance_data?.completed) {
+            alert('이미 학습이 완료되었습니다.');
+            return;
+        }
+
+        let delta = 10;
+        for (let time = 0; ; time += delta) {
+            const res = await fetch(`${targetUrl}&callback=aaa&state=8&duration=${duration}&currentTime=${time}&cumulativeTime=${time}`); // todo: duration 정하기
+            console.log({time, delta});
+            if (!res.ok) {
+                console.log('response failed', {time, delta});
+                break;
+            }
+            const text = await res.text();
+            const json = JSON.parse(text.replace('aaa(', '').replace(')', ''));
+            if (json.error_code) {
+                console.warn('error:', json.error_code, {time, delta});
+                delta = 10;
+                time /= 2;
+            } else {
+                delta += 10;
+            }
+
+            const c = await check();
+            if (c.attendance_data?.completed) {
+                alert('학습이 완료되었습니다.');
+                break;
+            }
+        }
+    };
+    root.appendChild(completeBtn);
 })();
-
-// GET https://canvas.ssu.ac.kr/learningx/api/v1/courses/44036/sections/0/components/794113/progress?user_id=20222904&content_id=69bd616f92eb6&content_type=movie&callback=jQuery111103847674061602282_1774676327407&state=8&duration=2428.16&currentTime=292.704&cumulativeTime=292.704&page=0&totalpage=0&cumulativePage=&_=1774676327410
-
-// fetch(`https://canvas.ssu.ac.kr/learningx/api/v1/courses/44036/sections/0/components/785670/progress?user_id=20222904&content_id=65f7de616e625&content_type=everlec&callback=jQuery111105042207917339521_1774156359305&state=3&duration=2675.6&currentTime=1688.63&cumulativeTime=2600.48&page=0&totalpage=0&cumulativePage=&_=1774156359306`)
-// fetch(`https://canvas.ssu.ac.kr/learningx/api/v1/courses/44036/sections/0/components/785670/progress?user_id=20222904&content_id=65f7de616e625&callback=aaa&state=3&duration=1&currentTime=0&cumulativeTime=2612.48`)
-// https://commons.ssu.ac.kr/em/65f7de616e625?startat=1688.63&endat=2600.48&TargetUrl=https%3A%2F%2Fcanvas.ssu.ac.kr%2Flearningx%2Fapi%2Fv1%2Fcourses%2F44036%2Fsections%2F0%2Fcomponents%2F785670%2Fprogress%3Fuser_id%3D20222904%26content_id%3D65f7de616e625%26content_type%3Deverlec&sl=1&pr=1&mxpr=2.00&lg=ko
-// https://canvas.ssu.ac.kr/learningx/api/v1/courses/44036/sections/0/components/785670/progress?user_id=20222904&content_id=65f7de616e625&content_type=everlec
-
 
 // file
 // POST https://canvas.ssu.ac.kr/learningx/api/v1/courses/44036/sections/0/components/794190/progress/forceSubmit?user_id=37567&user_login=20222904&content_id=${content_id}&content_type=file
