b898ec8e7679ecd12b76fb1f766efb17ac757ce1
cometj03 <harryjung1001@gmail.com>
2026-04-04 18:07:23 +0900

    시간 증가 방식 수정

--- content-script.js
+++ content-script.js
@@ -14,6 +14,8 @@ const DEBUG = true;
     if (DEBUG) console.log('root', root);
     if (!root) return;
 
+    // wait for videoIframe loaded
+    // TODO: 영상 강의가 아닌 경우 videoIframe이 없음 -> 종료시키기
     const videoIframe = await new Promise((resolve) => {
         const observer = new MutationObserver((mutations, obs) => {
             const videoIframe = root.querySelector('div > div.xnlail-video-component > div.xnlailvc-commons-container > iframe');
@@ -27,9 +29,9 @@ const DEBUG = true;
     if (DEBUG) console.log('videoIframe', videoIframe);
 
     const videoSrc = videoIframe.getAttribute('src');
-    if (DEBUG) console.log('videoSrc', videoSrc); // ex) ex) https://commons.ssu.ac.kr/em/69bd616f92eb6?startat=0.00&endat=0.00&TargetUrl=https%3A%2F%2Fcanvas.ssu.ac.kr%2Flearningx%2Fapi%2Fv1%2Fcourses%2F44036%2Fsections%2F0%2Fcomponents%2F794113%2Fprogress%3Fuser_id%3D20222904%26content_id%3D69bd616f92eb6%26content_type%3Dmovie&sl=1&pr=1&mxpr=1.00&lg=ko
+    if (DEBUG) console.log('videoSrc', videoSrc); // videoSrc ex) https://commons.ssu.ac.kr/em/69bd616f92eb6?startat=0.00&endat=0.00&TargetUrl=https%3A%2F%2Fcanvas.ssu.ac.kr%2Flearningx%2Fapi%2Fv1%2Fcourses%2F44036%2Fsections%2F0%2Fcomponents%2F794113%2Fprogress%3Fuser_id%3D20222904%26content_id%3D69bd616f92eb6%26content_type%3Dmovie&sl=1&pr=1&mxpr=1.00&lg=ko
     const targetUrl = new URL(videoSrc).searchParams.get('TargetUrl');
-    if (DEBUG) console.log('targetUrl', targetUrl); // ex) https://canvas.ssu.ac.kr/learningx/api/v1/courses/44036/sections/0/components/794113/progress?user_id=20222904&content_id=69bd616f92eb6&content_type=movie
+    if (DEBUG) console.log('targetUrl', targetUrl); // targetUrl ex) https://canvas.ssu.ac.kr/learningx/api/v1/courses/44036/sections/0/components/794113/progress?user_id=20222904&content_id=69bd616f92eb6&content_type=movie
 
     const courseId = root.getAttribute('data-course_id');
     const itemId = root.getAttribute('data-item_id');
@@ -43,9 +45,13 @@ const DEBUG = true;
                 'Authorization': `Bearer ${token}`
             }
         });
-        if (!res.ok) return {};
+        if (!res.ok) {
+            console.warn('check: response failed', res);
+            return {};
+        }
         return await res.json();
     };
+
     const completeBtn = document.createElement('button');
     completeBtn.innerText = '학습 완료시키기';
     completeBtn.onclick = async () => {
@@ -53,14 +59,17 @@ const DEBUG = true;
         if (DEBUG) console.log(c);
         const duration = c.item_content_data?.duration ?? 1;
         if (c.attendance_data?.completed) {
-            alert('이미 학습이 완료되었습니다.');
-            return;
+            if (!confirm('이미 학습이 완료되었습니다. 그럼에도 실행하시겠습니까?'))
+                return;
         }
 
-        let delta = 10;
-        for (let time = 0; ; time += delta) {
+        let time = c.attendance_data?.last_at ?? 0;
+        let delta = 60;
+        do {
+            time += delta;
+            if (time > duration) time = duration;
+            
             const res = await fetch(`${targetUrl}&callback=aaa&state=8&duration=${duration}&currentTime=${time}&cumulativeTime=${time}`); // todo: duration 정하기
-            console.log({time, delta});
             if (!res.ok) {
                 console.log('response failed', {time, delta});
                 break;
@@ -68,17 +77,21 @@ const DEBUG = true;
             const text = await res.text();
             const json = JSON.parse(text.replace('aaa(', '').replace(')', ''));
             if (json.error_code) {
-                console.warn('error:', json.error_code, {time, delta});
-                delta = 10;
-                time /= 2;
+                console.warn('error:', json.error_code, {time});
+                time -= delta;
+                delta = 60;
             } else {
-                delta += 10;
+                delta += 60;
             }
 
             const c = await check();
+            console.log(`${time / duration * 100}% 완료`, {completed: c.attendance_data?.completed, time, delta});
+        } while (time < duration);
+
+        {
+            const c = await check();
             if (c.attendance_data?.completed) {
                 alert('학습이 완료되었습니다.');
-                break;
             }
         }
     };
