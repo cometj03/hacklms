c00dbf661a74d6596927545768b62b10891984a0
cometj03 <harryjung1001@gmail.com>
2026-05-01 01:22:01 +0900

    fix: 학습 진행도 last_at 대신 progress 사용
    last_at은 마지막으로 본 위치이기 때문에 학습 진행도의 의미와 다름
    
    진행도를 progress라는 변수명으로 고침에 따라 getVideoProgress()를 getVideoStatus()로 이름 변경

--- background.js
+++ background.js
@@ -9,17 +9,17 @@ chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
         }
         case 'get-video-progress': {
             const {courseId, itemId, xn_api_token} = message.data;
-            const progress = await getVideoProgress(courseId, itemId, xn_api_token);
+            const status = await getVideoStatus(courseId, itemId, xn_api_token);
             sendMessageToPopup('set-video-progress', {
-                percent: progress.last_at / progress.duration * 100,
-                is_completed: progress.is_completed,
+                percent: status.progress / status.duration * 100,
+                is_completed: status.is_completed,
             });
             break;
         }
     }
 });
 
-async function getVideoProgress(courseId, itemId, token) {
+async function getVideoStatus(courseId, itemId, token) {
     const res = await fetch(`https://canvas.ssu.ac.kr/learningx/api/v1/courses/${courseId}/attendance_items/${itemId}`, {
         headers: {
             'Authorization': `Bearer ${token}`
@@ -32,17 +32,17 @@ async function getVideoProgress(courseId, itemId, token) {
     }
     const json = await res.json();
     const duration = json.item_content_data?.duration ?? 1; // 1 이상의 값이어야 함
-    const last_at = json.attendance_data?.last_at ?? 0;
+    const progress = json.attendance_data?.progress ?? 0;
     const is_completed = json.attendance_data?.completed ?? false;
-    return { duration, last_at, is_completed };
+    return { duration, progress, is_completed };
 }
 
 async function completeVideoProgress(data) {
     const {targetUrl, courseId, itemId, xn_api_token} = data;
     console.assert(targetUrl && courseId && itemId && xn_api_token, 'these params shoud be exist');
 
-    const {duration, last_at} = await getVideoProgress(courseId, itemId, xn_api_token);
-    let time = last_at;
+    const {duration, progress} = await getVideoStatus(courseId, itemId, xn_api_token);
+    let time = progress;
     let delta = 60;
     do {
         time += delta;
@@ -63,10 +63,10 @@ async function completeVideoProgress(data) {
             delta += 60;
         }
 
-        const progress = await getVideoProgress(courseId, itemId, xn_api_token);
+        const status = await getVideoStatus(courseId, itemId, xn_api_token);
         sendMessageToPopup('set-video-progress', {
             percent: time / duration * 100, 
-            is_completed: progress.is_completed,
+            is_completed: status.is_completed,
         });
         console.log(`${time / duration * 100}% 완료`, {time, delta});
     } while (time < duration);
@@ -75,24 +75,6 @@ async function completeVideoProgress(data) {
 function sendMessageToPopup(type, data) {
     chrome.runtime.sendMessage({target: 'popup', type, data});
 }
-
-// video download
-/*
-chrome.downloads.download({
-    url: request.url,
-    filename: "ssmovie.mp4",
-    headers: [
-    { name: "Referer", value: "https://commons.ssu.ac.kr/" }
-    ],
-    saveAs: true // 사용자가 저장 위치를 선택하게 함
-}, (downloadId) => {
-    if (chrome.runtime.lastError) {
-    sendResponse({ status: "error", message: chrome.runtime.lastError.message });
-    } else {
-    sendResponse({ status: "success", id: downloadId });
-    }
-});    
-*/
 
 // 배속 방지 리스너 제거
 // video.removeEventListener('ratechange', getEventListeners(video).ratechange[0].listener)
